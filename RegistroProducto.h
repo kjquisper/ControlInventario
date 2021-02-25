6d27f8ab8777cdd504c900cf4db30ae1e1bd3ddf
kjquisper <78871969+kjquisper@users.noreply.github.com>
2021-02-24 19:25:10 -0500

    Update RegistroProducto.h

---
+++ RegistroProducto.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class RegistrarProducto{
+    private:
+        int Cantidad;
+        bool Estado;
+        //Empleado *Empleado;
+        string FechaHora;
+    public:
+        void MostrarRegistro();
+};
