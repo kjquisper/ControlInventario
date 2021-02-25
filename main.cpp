72fd68a6f772dc66b2a3bf8c87ec0a790918a8b5
kjquisper <78871969+kjquisper@users.noreply.github.com>
2021-02-24 19:25:16 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
-#include <iostream>
+#include <Usuario.h>
 
 using namespace std;
 
 
 
 int main(){
+    cout<<"\t\t\tLogin\t";
+    cout<<"\n\t\t\t--------";
+
+    if(){
 
 
-
+    }else
 
     return 0;
 }
