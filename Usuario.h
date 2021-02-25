9208e17735a99300f211827a5457f461afd943eb
kjquisper <78871969+kjquisper@users.noreply.github.com>
2021-02-24 19:15:01 -0500

    Create Usuario.h

---
+++ Usuario.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+ class Usuario {
+     private:
+        string usuario;
+        string contraseña;
+        string DNI;
+        string login;
+     public:
+         void Mostrar_usuario();
+};
+
+
+
