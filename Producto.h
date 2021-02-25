597523c54443896df739bcdafc66a3c4ba3f94ca
kjquisper <78871969+kjquisper@users.noreply.github.com>
2021-02-24 19:15:09 -0500

    Create Producto.h

---
+++ Producto.h
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Producto {
+    private:
+        char codigo;
+        string Nombre;
+        int stock;
+        string categoria;
+        float precio;
+        //Proveedor *proveedor;
+    public:
+        Producto();
+        void Registrar_Entrada(int);
+        void Registrar_Salida(int);
+        void Actualizar_Stock();
+        void MostrarProducto();
+        void ModificarProducto();
+};
+
+Producto :: Producto(){
+    
+
+}
