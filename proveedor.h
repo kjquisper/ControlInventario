c8f72e00f294b20339ccc7afd307ad5611717bb8
kjquisper <78871969+kjquisper@users.noreply.github.com>
2021-02-24 19:15:15 -0500

    Create proveedor.h

---
+++ proveedor.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class proveedor {
+    private:
+        string Nombre;
+        string Referencia;
+        //producto *productos;
+        //empleados *empleado;
+    public:
+        void MostrarProducto();
+        //void Mostrarempleado();
+        void MostrarProveedor();
+        void AsignarEmpleado(empleado: Empleado);
+};
