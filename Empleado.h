e9629d9e6347b1edd8de8fe67ffca761c56ed869
yino soncco <yinosoncco27@gmail.com>
2021-02-27 14:49:48 -0500

    .

--- Empleado.h
+++ Empleado.h
@@ -1,13 +1,17 @@
+# ifndef EMPLEADO_H
+# define EMPLEADO_H
+
 #include <iostream>
 #include <string>
-#include <Usuario.h>
+#include "Usuario.h"
 
 using namespace std;
 
 class Empleado : public Usuario{
     private:
-    
-
+        Proveedor * proveedor;
     public:
-
+      void Operacion(string ,bool ,int);
 };
+
+#endif // EMPLEADO_H
