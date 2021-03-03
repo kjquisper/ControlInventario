8e330db3c83c9434f52a8955785ed7f0502715eb
kjquisper <78871969+kjquisper@users.noreply.github.com>
2021-03-02 19:02:49 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,18 +7,20 @@ using namespace std;
 
 int main(){
     Usuario Colb("Juan","micasa07","71216426");
-
-    cout<<"Login\n";
-    cout<<"Usuario: "<<endl;
-    cout<<"Contraseña: "<<endl;
-
-    if(Colb.Acceso()==true)
-    {
-        cout<<"\nLogeado correctamente!";
-    }else
-    {
-       cout<<"\nLogeo incorrecto!";
-    }
-
+    int a=1;
+	while(a)
+	{
+	system("cls");
+    cout<<"Login\n"<<endl;
+    
+	    if(Colb.Acceso()==true)
+	    {
+	        cout<<"\nLogeado correctamente!";
+	        break;
+	    }else{
+		
+	       cout<<"\nLogeo incorrecto!"<<endl;
+		}
+	}
     return 0;
 }
