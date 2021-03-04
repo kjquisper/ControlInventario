c6e8035e9798f9b95fbbddfb3fcecd956e20d610
kjquisper <78871969+kjquisper@users.noreply.github.com>
2021-03-03 19:00:42 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,87 @@
-#include<iostream>
-#include "Usuario.h"
-
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+#include "Empleado.h"
+#include "Administrador.h"
 using namespace std;
-
+void menuempleado();
+void menuadmi();
 
 
 int main(){
-    Usuario Colb("Juan","micasa07","71216426");
-    int a=1;
-	while(a)
-	{
-	system("cls");
-    cout<<"Login\n"<<endl;
+    Empleado Colb1("Juan","miaula07","712164");
+    Empleado Colb2("luis","miaula08","743567");
+    Administrador Admi("Andre","admi1","723456");
     
-	    if(Colb.Acceso()==true)
-	    {
+	int a=1,op;
+	
+	do
+	{
+		system("CLS");
+	cout<<"\t\tSecion\n"<<endl;
+	cout<<"1.Empleado"<<endl;
+	cout<<"2.Administrador"<<endl;
+	cout<<"OPCION:  ";cin>>op;
+	
+	switch(op){
+		
+		case 1:
+		while(a)
+				{
+				system("cls");
+    			cout<<"Login\n"<<endl;
+    
+	    	if(Colb1.Acceso()==true)
+	    	{
 	        cout<<"\nLogeado correctamente!";
-	        break;
-	    }else{
+	        menuempleado();
+	        
+	        
+	    	}else{
 		
 	       cout<<"\nLogeo incorrecto!"<<endl;
-		}
+			system("PAUSE");
+			}
+				}	
+		break;
+		
+		case 2:
+		while(a)
+				{
+				system("cls");
+    			cout<<"Login\n"<<endl;
+    
+	    	if(Admi.Acceso()==true)
+	    	{
+	        cout<<"\nLogeado correctamente!";
+	        break;
+	        
+	    	}else{
+		
+	       cout<<"\nLogeo incorrecto!"<<endl;
+			system("PAUSE");
+			}
+				}		
+		break;
 	}
+	
+	}while(op!=3);
+	
     return 0;
+}
+
+
+void menuempleado(){
+	do{
+		system("cls");
+		cout<<"\t\tActividades"<<endl;
+		cout<<"1.Mostrar Almacen"<<endl;
+		cout<<"2.Registrar Producto"<<endl;
+		cout<<"3."
+	switch(op){
+		
+		
+		
+		}	
+	}while(op!=);
 }
